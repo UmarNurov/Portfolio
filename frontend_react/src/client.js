32abb580427c19cdc006e1c447487f4411270a40
UmarNurov <umar.nurov.02@gmail.com>
2022-12-09 14:49:05 +0500

    New updates

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -2,11 +2,11 @@ import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  projectId: "gwgv483t",
   dataset: 'production',
   apiVersion: '2022-02-01',
   useCdn: true,
-  token: process.env.REACT_APP_SANITY_TOKEN,
+  token: "skqVj6mhSiXvoPbQ2wzMOvbRgt4pVRRAnGJfW6LtLp5F95BtJUh3FKnSOigwWzzSyYBq8D9cCeMkkB92LFwNOfwYaYhUqzLMzG7jCgbXz0t0fksF2h0YcxpLuVFFzyisJ3dkTOyjsG0bGItlLoOeZKdJebJ7efsDdbG2flz5E1WUjInqIMtX",
 });
 
 const builder = imageUrlBuilder(client);
